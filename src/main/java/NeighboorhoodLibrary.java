054d5b31b15d0daddf267fa51a53dae0d7bfe2dc
Bmorse-coder <bmorse@appdev.yearup.org>
2023-04-21 19:48:46 -0400

    Add new files

---
+++ src/main/java/NeighboorhoodLibrary.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class NeighboorhoodLibrary {
+    public static void main(String[] args) {
+        Scanner Book = new Scanner(System.in);
+
+        Book TheReaper = new Book(343634364, "978-3-16-148410-0", "The Reaper");
+        Book Tree = new Book(565646465,  "968-3-67-878410-0", "Tree");
+        Book TheMirror = new Book(443454435,  "678-3-16-165780-0", "The Mirror");
+        Book Fan = new Book(545545456,  "768-3-16-178540-0", "Fan");
+        Book Floor = new Book(645655776,  "677-3-16-187610-0", "Floor", );
+    }
+
+}
