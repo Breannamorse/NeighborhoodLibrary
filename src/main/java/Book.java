b719b1bc6508981519600ebdf35fc705c2f553be
Bmorse-coder <bmorse@appdev.yearup.org>
2023-04-25 21:54:24 -0400

    Add new files

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -3,46 +3,66 @@ public class Book {
     private String isbn;
     private String title;
     private boolean isCheckedOut;
-    private  String checkedOutTo;
+    private String checkedOutTo;
 
-    public Book(int id, String isbn, String title, boolean isCheckedOut,
-                String checkedOutTo) {
+    public Book(int id, String isbn, String title) {
         this.id = id;
         this.isbn = isbn;
         this.title = title;
-        this.isCheckedOut = isCheckedOut;
-        this.checkedOutTo = checkedOutTo;
+        this.isCheckedOut = false;
+        this.checkedOutTo = "";
     }
+
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
-    public String getIsbn(){
+
+    public String getIsbn() {
         return isbn;
     }
+
     public void setIsbn(String isbn) {
         this.isbn = isbn;
     }
+
     public String getTitle() {
         return title;
     }
+
     public void setTitle(String title) {
         this.title = title;
     }
+
     public boolean getCheckOut() {
         return isCheckedOut;
     }
+
     public void setCheckedOut(boolean isCheckedOut) {
         this.isCheckedOut = isCheckedOut;
     }
+
     public String getCheckedOutTo() {
         return checkedOutTo;
     }
+
     public void setCheckedOutTo(String checkedOutTo) {
         this.checkedOutTo = checkedOutTo;
     }
 
+
+    public void checkIn() {
+        this.checkedOutTo = "";
+        this.isCheckedOut = false;
+    }
+
+    public void checkOut(String name) {
+        this.checkedOutTo = name;
+        this.isCheckedOut = true;
+    }
 }
+
 
--- src/main/java/NeighboorhoodLibrary.java
+++ src/main/java/NeighboorhoodLibrary.java
@@ -1,14 +1,68 @@
 import java.util.Scanner;
 
 public class NeighboorhoodLibrary {
+    private static Book[] books = new Book[5];
+
     public static void main(String[] args) {
-        Scanner Book = new Scanner(System.in);
 
-        Book TheReaper = new Book(343634364, "978-3-16-148410-0", "The Reaper");
-        Book Tree = new Book(565646465,  "968-3-67-878410-0", "Tree");
-        Book TheMirror = new Book(443454435,  "678-3-16-165780-0", "The Mirror");
-        Book Fan = new Book(545545456,  "768-3-16-178540-0", "Fan");
-        Book Floor = new Book(645655776,  "677-3-16-187610-0", "Floor", );
+
+
+
+        Book book1 = new Book(3434, "454435", "The Reaper");
+        Book book2 = new Book(3434, "453465", "The Tree");
+        Book book3 = new Book(4555, "353545", "The Sea");
+        Book book4 = new Book(44545, "45455", "The Ocean");
+        Book book5 = new Book(445455, "4353445", "The Sand");
+
+        books[0] = book1;
+        books[1] = book2;
+        books[2] = book3;
+        books[3] = book4;
+        books[4] = book5;
+        Scanner scanner = new Scanner(System.in);
+
+        boolean checkedOut = false;
+        while (!checkedOut) {
+            System.out.println("Pick an option");
+            System.out.println("1 - Show Available Books");
+            System.out.println("2 - Show Checked Out Books");
+            System.out.println("Exit - Close out of application");
+
+            int command = scanner.nextInt();
+
+            scanner.nextLine();
+
+            switch (command) {
+                case 1:
+                    for (int i = 0; i < books.length; i++) {
+                        System.out.println(books[i].getId() + " | " + books[i].getIsbn() + " | " + books[i].getTitle());
+
+                    }
+                    System.out.println("Select an option");
+                    System.out.println("Select 1 to choose a book");
+                    System.out.println("Select 2 to exit to home screen");
+
+                    int Scanner = scanner.nextInt();
+                    if (Scanner.equals("1")) {
+                        System.out.println("Username: ");
+                        String userName = scanner.next();
+
+                        System.out.println("What book would you like?");
+                        int userBook = scanner.nextInt();
+
+                        for (int i = 0; i < books.length; i++) {
+                            if (userBook == books[i].getId()) {
+                                books[i].checkOut(userName);
+                                System.out.println("Book" + books[i].getTitle() + "Has been checked out by" + userName);
+                            }
+
+                        }
+                    }
+                    if (Scanner.equals(checkedOut))
+                        break;
+            }
+
+
+        }
     }
-
 }
